2ef12c889056e897e474fc444122f4c95ec4816b
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-03-03 16:02:54 +0100

    feat: updating AuthForm UI to be beautiful

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -8,6 +8,7 @@ interface AuthFormProps {
 
 const AuthForm = ({ isLogin = false }: AuthFormProps) => {
     const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
     const { login } = useAuth();
     const navigate = useNavigate();
 
@@ -19,20 +20,39 @@ const AuthForm = ({ isLogin = false }: AuthFormProps) => {
 
     return (
         <div className="p-4 max-w-md mx-auto border rounded shadow">
-            <h2 className="text-xl font-bold mb-4">{isLogin ? "Login" : "Register"}</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Nom"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="w-full p-2 border rounded mb-2"
-                    required
-                />
-                <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
+            <h2 className="text-xl font-bold mb-4">{isLogin ? "Connection" : "Inscription"}</h2>
+            <form onSubmit={handleSubmit} className="mb-3">
+
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">Nom d'utilisateur</label>
+                    <input
+                        type="text"
+                        placeholder="John Doe"
+                        className="form-control"
+                        id="username"
+                        aria-describedby="emailHelp"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
                     {isLogin ? "Se connecter" : "S'inscrire"}
                 </button>
             </form>
+
             <Link to={isLogin ? "/register" : "/login"} className="text-blue-500 text-sm">
                 {isLogin ? "Créer un compte" : "Déjà inscrit ? Connectez-vous"}
             </Link>
